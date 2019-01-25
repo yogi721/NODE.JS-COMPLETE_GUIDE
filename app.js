d139653e007bd02a0ce887933669edcb1b3c0d91
yogi721 <semlali.mehdi.1980@gmail.com>
2019-01-25 01:31:05 +0100

    Adding Routing

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const http = require('http');
 
 // create server: arrow function
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
+    
 });
 
 server.listen(3000);
